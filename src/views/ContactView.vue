b14e3de4fc47e8897e1674f2ba8ad3707d50f6af
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-02-17 11:20:34 +0600

    clear file and folder

--- src/views/ContactView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="text-center py-10">
-      <h1 class="text-3xl font-bold text-gray-800">About Us</h1>
-      <p class="text-gray-600 mt-2">This is the about page of our website.</p>
-    </div>
-  </template>
-  
--- src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="text-center py-10">
-    <h1 class="text-3xl font-bold text-gray-800">Welcome to Home Page</h1>
-  </div>
-</template>
